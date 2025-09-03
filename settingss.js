ad011afc4d7729227d993b134e18aebbf2a44908
dyekosi-debug <dyekosi@gmail.com>
2025-09-03 14:56:47 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~xm0DiDjS#WeAEhou372B-x_yrfXFXHQcDjEQ6szA4BxnuLjkITOY",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "256766104887", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
